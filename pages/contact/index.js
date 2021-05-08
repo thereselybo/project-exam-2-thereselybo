ae181065efdf30eabc649c39fe04d8f051d3cbd0
Therese <therese.lybo@gmail.com>
2021-05-08 09:48:13 +0200

    Write markup for contact and login pages

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,9 +1,68 @@
 import Layout from "../../components/layout/PublicLayout/PublicLayout";
+import Image from "next/image";
+
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
 
 const Contact = () => {
   return (
     <Layout title="Contact">
-      <h1>Contact</h1>
+      <Row className="d-flex flex-md-row justify-content-between">
+        {/* left side content */}
+        <Col md={6}>
+          <Container>
+            <h1>Contact</h1>
+            <Form>
+              <Row className="my-3">
+                <Form.Group as={Col} xs={6}>
+                  <Form.Label>First name</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+
+                <Form.Group as={Col} xs={6}>
+                  <Form.Label>Last name</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+
+                <Form.Group as={Col} xs={12}>
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" />
+                </Form.Group>
+
+                <Form.Group as={Col} xs={12}>
+                  <Form.Label>Topic</Form.Label>
+                  <Form.Control as="select" defaultValue="Choose...">
+                    <option>Choose...</option>
+                    <option>...</option>
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group as={Col} xs={12}>
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+              </Row>
+
+              <Col className="d-flex justify-content-center">
+                <Button variant="primary" size="lg" className="mx-auto mt-3">
+                  Send message
+                </Button>
+              </Col>
+            </Form>
+          </Container>
+        </Col>
+        {/* right side content */}
+        <Col
+          md={6}
+          className="d-none d-md-flex h-100 position-fixed end-0 top-0"
+        >
+          <Image
+            src="/images/contact.svg"
+            alt="Illustration of a laptop in front of tropical scenery"
+            layout="fill"
+            objectFit="cover"
+          />
+        </Col>
+      </Row>
     </Layout>
   );
 };
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,9 +1,50 @@
+import Image from "next/image";
 import Layout from "../../components/layout/PublicLayout/PublicLayout";
+
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
 
 const Login = () => {
   return (
     <Layout title="Login">
-      <h1>Login</h1>
+      <Row className="d-flex flex-md-row justify-content-between">
+        {/* left side content */}
+        <Col md={6}>
+          <Container>
+            <h1>Login</h1>
+            <Form>
+              <Row className="my-3">
+                <Form.Group as={Col} xs={12}>
+                  <Form.Label>Username/email</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+
+                <Form.Group as={Col} xs={12}>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" />
+                </Form.Group>
+              </Row>
+
+              <Col className="d-flex justify-content-center">
+                <Button variant="primary" size="lg" className="mx-auto mt-3">
+                  Login
+                </Button>
+              </Col>
+            </Form>
+          </Container>
+        </Col>
+        {/* right side content */}
+        <Col
+          md={6}
+          className="d-none d-md-flex h-100 position-fixed end-0 top-0"
+        >
+          <Image
+            src="/images/login.svg"
+            alt="Illustration of bungalow"
+            layout="fill"
+            objectFit="cover"
+          />
+        </Col>
+      </Row>
     </Layout>
   );
 };
